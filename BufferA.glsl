7535c4f69f69bae71c92a795d06028c0e63fc6ae
Dmitry Shesterkin <dfb@yandex.ru>
2022-05-27 00:49:37 -0700

    Polishing

--- BufferA.glsl
+++ BufferA.glsl
@@ -1,5 +1,8 @@
 #define C ivec2
 
+#define READ_STATE( uv, gen ) \
+    (int( texelFetch(iChannel0, (uv), 0)[(gen)] + 0.5))
+
 ivec2[] initState =  ivec2[] (
     C(50,180), C(51,180), C(50,181), C(51,181), C(60,180), 
     C(60,179), C(60,181), C(61,178), C(62,177), C(63,177), 
@@ -12,26 +15,15 @@ ivec2[] initState =  ivec2[] (
 ); 
 
 
-vec4 testColor(in vec2 fragCoord )
-{
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
-    
-    // Time varying pixel color
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));   
-
-    // Output to screen
-    return vec4(col,1.0);
-}
-
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     ivec2 uv = ivec2(fragCoord);
     
     if(iFrame == 0)
     {
+        ivec2 mUv = (uv + ivec2(40, 100)) % 200;
         for(int i=0;i != initState.length(); ++i)
-            if( uv == initState[i] )
+            if( mUv == initState[i] )
             {
                 fragColor = vec4(1.0,1.0,1.0,1.0);
                 return;
@@ -41,31 +33,28 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         return;
     }
 
-    int curGen = iFrame % 2;
-    int newGen = (iFrame + 1) % 2;
-        
-    int curState = int(texelFetch( iChannel0, uv, 0 )[curGen] + 0.5);
+    int curGen = iFrame % 2;   
+    int curState = READ_STATE( uv, curGen );
     
     int totalAliveAround = 0;
     
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2(-1, -1), 0 )[curGen] + 0.5);
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2( 0, -1), 0 )[curGen] + 0.5);
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2( 1, -1), 0 )[curGen] + 0.5);
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2(-1,  0), 0 )[curGen] + 0.5);
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2( 1,  0), 0 )[curGen] + 0.5);
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2(-1,  1), 0 )[curGen] + 0.5);
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2( 0,  1), 0 )[curGen] + 0.5);
-    totalAliveAround += int(texelFetch( iChannel0, uv + ivec2( 1,  1), 0 )[curGen] + 0.5);
+    totalAliveAround += READ_STATE( uv + ivec2(-1, -1), curGen );
+    totalAliveAround += READ_STATE( uv + ivec2( 0, -1), curGen );
+    totalAliveAround += READ_STATE( uv + ivec2( 1, -1), curGen );
+    totalAliveAround += READ_STATE( uv + ivec2(-1,  0), curGen );
+    totalAliveAround += READ_STATE( uv + ivec2( 1,  0), curGen );
+    totalAliveAround += READ_STATE( uv + ivec2(-1,  1), curGen );
+    totalAliveAround += READ_STATE( uv + ivec2( 0,  1), curGen );
+    totalAliveAround += READ_STATE( uv + ivec2( 1,  1), curGen );
      
     int newState = 0;
     
-    //https://conwaylife.com/wiki/Conway%27s_Game_of_Life
-    
+    //Rules: https://conwaylife.com/wiki/Conway%27s_Game_of_Life    
     switch( totalAliveAround )
     {
     case 2:
-        if( curState == 0 )
-            break;
+        newState = curState;
+        break;
         
     case 3:
         newState = 1;
@@ -74,10 +63,11 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     
     ivec2 res;
     
+    int newGen = (iFrame + 1) % 2;
+    
     res[newGen] = newState;
     res[curGen] = curState;
     
-    fragColor = vec4(vec2(res), fragColor.zw );
-    
+    fragColor = vec4(vec2(res), fragColor.zw );  
 }
 
