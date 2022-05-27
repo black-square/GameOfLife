c7ef3b761d5ee45d493a0a884687c5e8cc52fde6
Dmitry Shesterkin <dfb@yandex.ru>
2022-05-27 15:34:53 -0700

    * Branchless main loop * Cleanup * WrapAround functionality

--- BufferA.glsl
+++ BufferA.glsl
@@ -1,18 +1,41 @@
-#define C ivec2
+#if 1
+    #define WRAP_AROUND( uv, res ) \
+        (ivec2( mod(vec2(uv), vec2(res)) ))
+#else
+     #define WRAP_AROUND( uv, res ) (uv)
+#endif
 
 #define READ_STATE( uv, gen ) \
-    (int( texelFetch(iChannel0, (uv), 0)[(gen)] + 0.5))
+    ( int( texelFetch(iChannel0, WRAP_AROUND(uv, iChannelResolution[0]), 0)[(gen)] + 0.5))
 
-ivec2[] initState =  ivec2[] (
-    C(50,180), C(51,180), C(50,181), C(51,181), C(60,180), 
-    C(60,179), C(60,181), C(61,178), C(62,177), C(63,177), 
-    C(61,182), C(62,183), C(63,183), C(65,182), C(66,181), 
-    C(66,180), C(66,179), C(65,178), C(64,180), C(67,180), 
-    C(70,181), C(70,182), C(70,183), C(71,181), C(71,182), 
-    C(71,183), C(72,180), C(72,184), C(74,180), C(74,179), 
-    C(74,184), C(74,185), C(84,182), C(84,183), C(85,182), 
-    C(85,183)
-); 
+
+const ivec2[] initState = ivec2[] (
+    ivec2(50,180), ivec2(51,180), ivec2(50,181), ivec2(51,181), ivec2(60,180), 
+    ivec2(60,179), ivec2(60,181), ivec2(61,178), ivec2(62,177), ivec2(63,177), 
+    ivec2(61,182), ivec2(62,183), ivec2(63,183), ivec2(65,182), ivec2(66,181), 
+    ivec2(66,180), ivec2(66,179), ivec2(65,178), ivec2(64,180), ivec2(67,180), 
+    ivec2(70,181), ivec2(70,182), ivec2(70,183), ivec2(71,181), ivec2(71,182), 
+    ivec2(71,183), ivec2(72,180), ivec2(72,184), ivec2(74,180), ivec2(74,179), 
+    ivec2(74,184), ivec2(74,185), ivec2(84,182), ivec2(84,183), ivec2(85,182), 
+    ivec2(85,183)
+);
+
+vec4 initFromArray(in ivec2 uv)
+{
+    const int repeat = 200;
+    ivec2 mUv = (uv + ivec2(40, 100));
+
+    //Let's introduce some variability
+    ivec2 shift = mUv / repeat;
+    shift = ivec2(shift.x * shift.y, shift.x * shift.y);
+    mUv = mUv % repeat + shift;
+
+    for(int i=0; i != initState.length(); ++i)
+        if( mUv == initState[i] )
+            return vec4(1.0,1.0,1.0,1.0);
+
+    return vec4(0.0, 0.0, 0.0, 1.0);
+}
 
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
@@ -21,15 +44,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     
     if(iFrame == 0)
     {
-        ivec2 mUv = (uv + ivec2(40, 100)) % 200;
-        for(int i=0;i != initState.length(); ++i)
-            if( mUv == initState[i] )
-            {
-                fragColor = vec4(1.0,1.0,1.0,1.0);
-                return;
-            }
-        
-        fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        fragColor = initFromArray(uv);
         return;
     }
 
@@ -47,27 +62,14 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     totalAliveAround += READ_STATE( uv + ivec2( 0,  1), curGen );
     totalAliveAround += READ_STATE( uv + ivec2( 1,  1), curGen );
      
-    int newState = 0;
-    
-    //Rules: https://conwaylife.com/wiki/Conway%27s_Game_of_Life    
-    switch( totalAliveAround )
-    {
-    case 2:
-        newState = curState;
-        break;
-        
-    case 3:
-        newState = 1;
-        break;
-    }
-    
+    //Rules: https://conwaylife.com/wiki/Conway%27s_Game_of_Life 
+    int newState = int(totalAliveAround == 2) * curState + int(totalAliveAround == 3);  
+            
     ivec2 res;
-    
     int newGen = (iFrame + 1) % 2;
     
     res[newGen] = newState;
     res[curGen] = curState;
     
-    fragColor = vec4(vec2(res), fragColor.zw );  
+    fragColor = vec4(res, fragColor.zw );  
 }
-
--- Image.glsl
+++ Image.glsl
@@ -1,21 +1,18 @@
 vec4 testColor(in vec2 fragCoord )
 {
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
+    vec2 uv = fragCoord / iResolution.xy;
     
-    // Time varying pixel color
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));   
+    vec3 col = 0.8 + 0.2 * cos(iTime + uv.xyx + vec3(0,2,4) );   
 
-    // Output to screen
-    return vec4(col,1.0);
+    return vec4(col, 1.0);
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
 	//vec2 uv = (fragCoord.xy + vec2(0,1000)) / iResolution.xy/8.0;
-    vec2 uv = fragCoord.xy / iResolution.xy/1.5;
+    vec2 uv = fragCoord.xy / iResolution.xy/1.0;
     
-    float val = texture(iChannel0, uv )[iFrame %2];
+    float val = texture(iChannel0, uv)[iFrame %2];
     fragColor = vec4(testColor(fragCoord).xyz * val, 1.0);
 
     //vec2 mcords = mod(fragCoord/16.0f, 2.0f);
